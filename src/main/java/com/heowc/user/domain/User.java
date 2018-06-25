83840393fb8a60062594797a6dfc14ffc0d0e60f
heowc <heowc1992@gmail.com>
2018-06-25 19:16:59 +0900

    - 불필요한 import 제거

--- src/main/java/com/heowc/user/domain/User.java
+++ src/main/java/com/heowc/user/domain/User.java
@@ -5,12 +5,9 @@ import com.heowc.point.domain.PointHistory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
