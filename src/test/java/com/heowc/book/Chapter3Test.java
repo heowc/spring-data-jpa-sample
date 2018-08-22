f6df391e9646cb1117aa243c4cceec0469d89406
heowc <heowc1992@gmail.com>
2018-08-22 19:42:40 +0900

    [ADD] 영속성 컨텍스트의 특징

--- src/test/java/com/heowc/book/Chapter3Test.java
+++ src/test/java/com/heowc/book/Chapter3Test.java
@@ -1,6 +1,5 @@
 package com.heowc.book;
 
-import com.heowc.point.domain.Point;
 import com.heowc.user.domain.Address;
 import com.heowc.user.domain.User;
 import com.heowc.user.domain.UserRequest;
@@ -21,23 +20,20 @@ public class Chapter3Test {
     private TestEntityManager entityManager;
 
     @Test
-    public void entityManager() {
+    public void test_잘_가져왔나() {
         assertThat(entityManager).isNotNull();
     }
 
     @Test
-    public void lifecycle() {
-        // 비영속
+    public void test_엔티티_생명주기() {
         System.out.println("=============== 비영속 ===============");
         User user = new UserRequest("heowc1992", "!@#$%", "wonchul", "heo", "12345", "Asia", "Seoul").toUser();
 
-        // 영속
         System.out.println("=============== 영속 ===============");
         entityManager.persist(user);
         entityManager.flush();
         entityManager.clear();
 
-        // 준영속
         System.out.println("=============== 준영속 ===============");
         User heowc1992 = entityManager.find(User.class, "heowc1992");
         entityManager.detach(heowc1992);
@@ -45,11 +41,65 @@ public class Chapter3Test {
         entityManager.flush(); // not update
         entityManager.clear();
 
-        // 삭제
         System.out.println("=============== 삭제 ===============");
         heowc1992 = entityManager.find(User.class, "heowc1992");
         entityManager.remove(heowc1992);
         entityManager.flush();
     }
 
+    @Test
+    public void test_1차_캐시_및_동일성_보장() {
+        User user = new UserRequest("heowc1992", "!@#$%", "wonchul", "heo", "12345", "Asia", "Seoul").toUser();
+        entityManager.persist(user);
+
+        User user1 = entityManager.find(User.class, "heowc1992");
+        User user2 = entityManager.find(User.class, "heowc1992");
+
+        assertThat(user1).isEqualTo(user2);
+    }
+
+    @Test
+    public void test_지연_등록() {
+        User user = new UserRequest("heowc1992", "!@#$%", "wonchul", "heo", "12345", "Asia", "Seoul").toUser();
+        User user2 = new UserRequest("heowc", "!@#$%", "wonchul", "heo", "12345", "Asia", "Seoul").toUser();
+
+        entityManager.persist(user);
+        entityManager.persist(user2);
+        System.out.println("=============== 아직 insert 안됨 ===============");
+
+        entityManager.flush();
+
+        /*
+
+        =============== 아직 insert 안됨 ===============
+        Hibernate:
+            insert
+            into
+        ...
+
+         */
+    }
+
+    @Test
+    public void test_변경_감지() {
+        User user1 = new UserRequest("heowc1992", "!@#$%", "wonchul", "heo", "12345", "Asia", "Seoul").toUser();
+        User savedUser1 = entityManager.persist(user1);
+        savedUser1.changeAddress(new Address("12345", "Asia", "Busan")); // update 발생 함
+
+        User user2 = new UserRequest("heowc", "!@#$%", "wonchul", "heo", "12345", "Asia", "Seoul").toUser();
+        User savedUser2 = entityManager.persist(user2);
+        savedUser2.changeAddress(new Address("12345", "Asia", "Busan"));
+        savedUser2.changeAddress(new Address("12345", "Asia", "Seoul")); // update 발생 안함
+
+        entityManager.flush();
+        entityManager.clear();
+
+        User heowc1992 = entityManager.find(User.class, "heowc1992");
+        User heowc = entityManager.find(User.class, "heowc");
+
+        assertThat(heowc1992).isNotNull();
+        assertThat(heowc).isNotNull();
+        assertThat(heowc1992.getAddress().getAddress2()).isEqualTo("Busan");
+        assertThat(heowc.getAddress().getAddress2()).isEqualTo("Seoul");
+    }
 }
